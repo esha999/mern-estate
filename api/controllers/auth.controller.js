979847e341e06dde5e338a7a54082d67a6022679
esha999 <ishu301999@gmail.com>
2025-04-11 18:48:53 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,5 +13,28 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error); // for middleware so that we do not need to pass message again and again
     //next(errorHandler(550, "error from the function")); // suppose we need to create error and pass our own code and message then we create error file called error.js and pass it over there.
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...restInfo } = validUser._doc; // it will separate the password from rest info of user as we don't want to send password to user
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restInfo);
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin); // these signin and signup names are for backend understand, For frontend it is sign-up and sign-in
 
 export default router;
