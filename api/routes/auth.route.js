58cee2b2dae5f0cd345407ea2b7eaef395c479a6
esha999 <ishu301999@gmail.com>
2025-04-17 04:01:48 +0530

    add sign out functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signin, signup, google } from "../controllers/auth.controller.js";
+import {
+  signin,
+  signup,
+  google,
+  signout,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin); // these signin and signup names are for backend understand, For frontend it is sign-up and sign-in
 router.post("/google", google);
+router.get("/signout", signout);
 
 export default router;
