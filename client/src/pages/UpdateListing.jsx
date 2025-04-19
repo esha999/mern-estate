153586f2a512c39e1bda4cebc61eb8c343114800
esha999 <ishu301999@gmail.com>
2025-04-20 00:48:02 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -311,6 +311,9 @@ export default function CreateListing() {
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
                 <span className="text-xs">($ / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -327,6 +330,9 @@ export default function CreateListing() {
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
                   <span className="text-xs">($ / month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
