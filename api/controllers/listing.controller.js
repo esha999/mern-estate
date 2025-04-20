a5e6a24bda77c879fe985c49b366599946d9eceb
esha999 <ishu301999@gmail.com>
2025-04-20 16:00:10 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -59,3 +59,57 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    // if user pass any limit then show that items otherwise by default show 9 listings.
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      // if offer is undefined or false, users wants to see everything: Listing with offer as well as without one also.
+      // But if offer is true, then they want to see only listings which has offers.
+
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //regex is inbuilt functionality of mongodb, so if we are using it in name it will search in words, part of words etc
+      // $option : i means don't care about lower or upper case, it gives every result
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    // skip startIndex means if it starts from 0  it will start from beg. but if start from 1 then it will ignore first 9 for us
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
