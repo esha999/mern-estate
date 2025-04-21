d12c8f6b997d40410b9c5ee945912a0e87666900
esha999 <ishu301999@gmail.com>
2025-04-22 01:15:08 +0530

    add pre installation

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,114 +1,101 @@
-import { useEffect, useState } from "react";
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from "firebase/storage";
-import { app } from "../firebase";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
-export default function CreateListing() {
-  const { currentUser } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const params = useParams();
+export default function UpdateListing() {
   const [files, setFiles] = useState([]);
+  const [uploadError, setUploadError] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     imageUrls: [],
     name: "",
     description: "",
     address: "",
-    type: "rent",
-    bedrooms: 1,
-    bathrooms: 1,
-    regularPrice: 50,
+    regularPrice: 100,
     discountPrice: 0,
-    offer: false,
-    parking: false,
+    bathRooms: 1,
+    bedRooms: 1,
     furnished: false,
+    parking: false,
+    type: "rent",
+    offer: false,
   });
-  const [imageUploadError, setImageUploadError] = useState(false);
-  const [uploading, setUploading] = useState(false);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const params = useParams();
 
-  useEffect(() => {
-    const fetchListing = async () => {
-      const listingId = params.listingId;
-      const res = await fetch(`/api/listing/get/${listingId}`);
-      const data = await res.json();
-      if (data.success === false) {
-        console.log(data.message);
-        return;
-      }
-      setFormData(data);
-    };
-
-    fetchListing();
-  }, []);
-
-  const handleImageSubmit = (e) => {
+  function handleImageSubmit() {
+    setError("");
+    setUploading(true);
+    if (files.length === 0) {
+      setUploadError("Please Choose an image to Upload");
+      setUploading(false);
+      return;
+    }
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
-      setUploading(true);
-      setImageUploadError(false);
       const promises = [];
 
       for (let i = 0; i < files.length; i++) {
         promises.push(storeImage(files[i]));
       }
-      Promise.all(promises)
-        .then((urls) => {
-          setFormData({
-            ...formData,
-            imageUrls: formData.imageUrls.concat(urls),
-          });
-          setImageUploadError(false);
-          setUploading(false);
-        })
-        .catch((err) => {
-          setImageUploadError("Image upload failed (2 mb max per image)");
-          setUploading(false);
+      Promise.all(promises).then((urls) => {
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.concat(urls),
         });
+        setUploading(false);
+      });
     } else {
-      setImageUploadError("You can only upload 6 images per listing");
+      setUploadError("You can only upload 6 images per listing");
       setUploading(false);
     }
-  };
+  }
 
-  const storeImage = async (file) => {
+  function storeImage(file) {
     return new Promise((resolve, reject) => {
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL);
-          });
-        }
-      );
-    });
-  };
+      const data = new FormData();
+      data.append("file", file);
+      data.append("upload_preset", import.meta.env.VITE_UPLOAD_PRESET);
+      data.append("cloud_name", import.meta.env.VITE_CLOUD_NAME);
 
-  const handleRemoveImage = (index) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open(
+        "POST",
+        "https://api.cloudinary.com/v1_1/day0vpx3d/image/upload"
+      );
+
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          resolve(response.url);
+        } else {
+          setUploadError(`Upload failed with status: ${xhr.status}`);
+          reject(`Upload failed with status: ${xhr.status}`);
+        }
+      };
+
+      xhr.onerror = () => {
+        setUploadError("An error occurred during the upload.");
+        reject("An error occurred during the upload.");
+      };
+
+      xhr.send(data);
+    });
+  }
+
+  function handleRemoveImage(index) {
     setFormData({
       ...formData,
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
-  };
+  }
 
-  const handleChange = (e) => {
+  function handleChange(e) {
+    setError("");
+
     if (e.target.id === "sale" || e.target.id === "rent") {
       setFormData({
         ...formData,
@@ -137,17 +124,17 @@ export default function CreateListing() {
         [e.target.id]: e.target.value,
       });
     }
-  };
+  }
 
-  const handleSubmit = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
     try {
       if (formData.imageUrls.length < 1)
-        return setError("You must upload at least one image");
+        return setError("You must upload atleast one image");
       if (+formData.regularPrice < +formData.discountPrice)
-        return setError("Discount price must be lower than regular price");
+        return setError("Discounted Price must be lower than regular price");
       setLoading(true);
-      setError(false);
+
       const res = await fetch(`/api/listing/update/${params.listingId}`, {
         method: "POST",
         headers: {
@@ -158,6 +145,7 @@ export default function CreateListing() {
           userRef: currentUser._id,
         }),
       });
+
       const data = await res.json();
       setLoading(false);
       if (data.success === false) {
@@ -168,15 +156,37 @@ export default function CreateListing() {
       setError(error.message);
       setLoading(false);
     }
-  };
+  }
+
+  function fileInput() {
+    setUploadError("");
+    setError("");
+  }
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const listingId = params.listingId;
+      const res = await fetch(`/api/listing/get/${listingId}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+      }
+      setFormData(data);
+    };
+    fetchListing();
+  }, []);
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
         Update a Listing
       </h1>
+
       <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
+            onChange={handleChange}
+            value={formData.name}
             type="text"
             placeholder="Name"
             className="border p-3 rounded-lg"
@@ -184,155 +194,152 @@ export default function CreateListing() {
             maxLength="62"
             minLength="10"
             required
-            onChange={handleChange}
-            value={formData.name}
           />
           <textarea
+            onChange={handleChange}
+            value={formData.description}
             type="text"
             placeholder="Description"
             className="border p-3 rounded-lg"
             id="description"
             required
-            onChange={handleChange}
-            value={formData.description}
           />
           <input
+            onChange={handleChange}
+            value={formData.address}
             type="text"
             placeholder="Address"
             className="border p-3 rounded-lg"
             id="address"
             required
-            onChange={handleChange}
-            value={formData.address}
           />
+
           <div className="flex gap-6 flex-wrap">
             <div className="flex gap-2">
               <input
+                onChange={handleChange}
+                checked={formData.type === "sale"}
                 type="checkbox"
                 id="sale"
                 className="w-5"
-                onChange={handleChange}
-                checked={formData.type === "sale"}
               />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
               <input
+                onChange={handleChange}
+                checked={formData.type === "rent"}
                 type="checkbox"
                 id="rent"
                 className="w-5"
-                onChange={handleChange}
-                checked={formData.type === "rent"}
               />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
               <input
+                onChange={handleChange}
+                checked={formData.parking}
                 type="checkbox"
                 id="parking"
                 className="w-5"
-                onChange={handleChange}
-                checked={formData.parking}
               />
-              <span>Parking spot</span>
+              <span>Parking Spot</span>
             </div>
             <div className="flex gap-2">
               <input
+                onChange={handleChange}
+                checked={formData.furnished}
                 type="checkbox"
                 id="furnished"
                 className="w-5"
-                onChange={handleChange}
-                checked={formData.furnished}
               />
               <span>Furnished</span>
             </div>
             <div className="flex gap-2">
               <input
+                onChange={handleChange}
+                checked={formData.offer}
                 type="checkbox"
                 id="offer"
                 className="w-5"
-                onChange={handleChange}
-                checked={formData.offer}
               />
               <span>Offer</span>
             </div>
           </div>
+
           <div className="flex flex-wrap gap-6">
             <div className="flex items-center gap-2">
               <input
+                onChange={handleChange}
+                value={formData.bedRooms}
                 type="number"
-                id="bedrooms"
+                id="bedRooms"
                 min="1"
                 max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
-                onChange={handleChange}
-                value={formData.bedrooms}
               />
               <p>Beds</p>
             </div>
             <div className="flex items-center gap-2">
               <input
+                onChange={handleChange}
+                value={formData.bathRooms}
                 type="number"
-                id="bathrooms"
+                id="bathRooms"
                 min="1"
                 max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
-                onChange={handleChange}
-                value={formData.bathrooms}
               />
               <p>Baths</p>
             </div>
             <div className="flex items-center gap-2">
               <input
+                onChange={handleChange}
+                value={formData.regularPrice}
                 type="number"
                 id="regularPrice"
                 min="50"
-                max="10000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
-                onChange={handleChange}
-                value={formData.regularPrice}
               />
               <div className="flex flex-col items-center">
-                <p>Regular price</p>
-                {formData.type === "rent" && (
-                  <span className="text-xs">($ / month)</span>
-                )}
+                <p>Regular Price</p>
+                <span className="text-xs">($ / month)</span>
               </div>
             </div>
             {formData.offer && (
               <div className="flex items-center gap-2">
                 <input
+                  onChange={handleChange}
+                  value={formData.discountPrice}
                   type="number"
                   id="discountPrice"
                   min="0"
-                  max="10000000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
-                  onChange={handleChange}
-                  value={formData.discountPrice}
                 />
                 <div className="flex flex-col items-center">
-                  <p>Discounted price</p>
-                  {formData.type === "rent" && (
-                    <span className="text-xs">($ / month)</span>
-                  )}
+                  <p>Discounted Price</p>
+                  <span className="text-xs">($ / month)</span>
                 </div>
               </div>
             )}
           </div>
         </div>
+
         <div className="flex flex-col flex-1 gap-4">
           <p className="font-semibold">
             Images:
             <span className="font-normal text-gray-600 ml-2">
-              The first image will be the cover (max 6)
+              The first Image will be the Cover (max 6)
             </span>
           </p>
+
           <div className="flex gap-4">
             <input
+              onClick={fileInput}
               onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
@@ -341,17 +348,15 @@ export default function CreateListing() {
               multiple
             />
             <button
-              type="button"
               disabled={uploading}
+              type="button"
               onClick={handleImageSubmit}
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
               {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
-          <p className="text-red-700 text-sm">
-            {imageUploadError && imageUploadError}
-          </p>
+          <p className="text-red-700 text-sm">{uploadError && uploadError}</p>
           {formData.imageUrls.length > 0 &&
             formData.imageUrls.map((url, index) => (
               <div
@@ -360,12 +365,12 @@ export default function CreateListing() {
               >
                 <img
                   src={url}
-                  alt="listing image"
+                  alt="listing-image"
                   className="w-20 h-20 object-contain rounded-lg"
                 />
                 <button
-                  type="button"
                   onClick={() => handleRemoveImage(index)}
+                  type="button"
                   className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
                 >
                   Delete
@@ -373,10 +378,10 @@ export default function CreateListing() {
               </div>
             ))}
           <button
-            disabled={loading || uploading}
+            disabled={uploading || loading}
             className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
           >
-            {loading ? "Updating..." : "Update listing"}
+            {loading ? "Updating..." : "Update Listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
